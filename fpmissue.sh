84b61f724f13d84120e0dcc3f62a3ab90913ac57
AhmadSamiKhan <ahmad.cloudways@gmail.com>
2021-01-18 17:37:24 +0500

    Create fpmissue.sh

---
+++ fpmissue.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PHP_VERSION=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,2,3)
+
+sed -i 's#pid = /run/php/php'"$PHP_VERSION"'-fpm.pid#pid = /var/run/php-fpm.pid#g' php-fpm.conf /etc/php/$PHP_VERSION/fpm/php-fpm.conf
+
+/etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart
