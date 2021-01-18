8a6cc58192136ed99b6e55c834403a8f81e90455
AhmadSamiKhan <ahmad.cloudways@gmail.com>
2021-01-18 17:37:05 +0500

    Delete fpm.sh

--- fpm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PHP_VERSION=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,2,3)
-
-sed 's#pid = /run/php/php'"$PHP_VERSION"'-fpm.pid#pid = /var/run/php-fpm.pid#g' php-fpm.conf /etc/php/$PHP_VERSION/fpm/php-fpm.conf
-
-/etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart
